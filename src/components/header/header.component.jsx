b6ce5b404621cf0e3c729f3764b3b100b8a73187
Neelesh <nt00605@gmail.com>
2020-02-10 22:19:11 +0530

    changing scss components to css in js components using styled compoenets library

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
@@ -10,26 +9,31 @@ import { selectCurrentUser } from '../../redux/user/user.selectors';
 import CartDropdown from '../cart-dropdown/cart.dropdown.component';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
-import './header.styles.scss';
+import { 
+    HeaderContainer, 
+    LogoContainer, 
+    OptionsContainer, 
+    OptionsLink 
+} from './header.styles';
 
 const Header = ({ currentUser, hidden }) => (
-    <div className="header">
-        <Link className="logo-container" to="/">
+    <HeaderContainer>
+        <LogoContainer to="/">
             <Logo className="logo"/>
-        </Link>
-        <div className="options">
-            <Link className="option" to="/shop">Shop</Link>
-            <Link className="option" to="/shop">Contact</Link>
+        </LogoContainer>
+        <OptionsContainer>
+            <OptionsLink to="/shop">Shop</OptionsLink>
+            <OptionsLink to="/shop">Contact</OptionsLink>
             {
                 currentUser ?
-                <div className="option" onClick={() => auth.signOut()}>Sign Out</div>
+                <OptionsLink as='div' onClick={() => auth.signOut()}>Sign Out</OptionsLink>
                 : 
-                <Link className="option" to="/signin">Sign In</Link>
+                <OptionsLink to="/signin">Sign In</OptionsLink>
             }
             <CartIcon />
-        </div>
+        </OptionsContainer>
         { hidden ? null : <CartDropdown />}
-    </div>
+    </HeaderContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
